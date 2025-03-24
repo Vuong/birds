31cb1f7df6c3473e58271fd5b089885ea1600197
vuong <vuongdo_777@yahoo.com>
2025-03-24 21:34:47 +0000

    Update database - fixing a bug

--- lib/version.ts
+++ lib/version.ts
@@ -7,10 +7,10 @@ import { build } from './modules'
 export async function version() {
   await build()
 
-  let pkgFile = path.resolve(rootDir, 'package.json')
-  let pkg = JSON.parse(await fsp.readFile(pkgFile, 'utf8'))
+  const pkgFile = path.resolve(rootDir, 'package.json')
+  const pkg = JSON.parse(await fsp.readFile(pkgFile, 'utf8'))
 
-  let hash = await hashFile(distFile).then(r => r.substr(0, 6))
+  const hash = await hashFile(distFile).then(r => r.substr(0, 6))
 
   pkg.version = pkg.version.split('-')[0] + '-' + hash
 
