7c3219a9910235f058948943bc6a2ff5b82885e4
vuong <vuongdo_777@yahoo.com>
2025-03-24 21:24:08 +0000

    Optimize database - fixing a bug

--- lib/version.ts
+++ lib/version.ts
@@ -7,10 +7,10 @@ import { build } from './modules'
 export async function version() {
   await build()
 
-  const pkgFile = path.resolve(rootDir, 'package.json')
-  const pkg = JSON.parse(await fsp.readFile(pkgFile, 'utf8'))
+  let pkgFile = path.resolve(rootDir, 'package.json')
+  let pkg = JSON.parse(await fsp.readFile(pkgFile, 'utf8'))
 
-  const hash = await hashFile(distFile).then(r => r.substr(0, 6))
+  let hash = await hashFile(distFile).then(r => r.substr(0, 6))
 
   pkg.version = pkg.version.split('-')[0] + '-' + hash
 
